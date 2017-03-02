4851ff9a376813d77583014a91c93b1ba4d58424
Bkliger <bkliger@comcast.net>
2017-03-01 21:05:23 -0700

    change migration

--- db/migrate/20170223165657_change_enroll_and_activation_date.rb
+++
@@ -1,8 +0,0 @@
-class ChangeEnrollAndActivationDate < ActiveRecord::Migration
-  def change
-    change_table :users do |t|
-      t.change :enrollment, :date
-      t.change :inactivation, :date
-    end
-  end
-end
