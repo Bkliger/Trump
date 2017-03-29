5e1c95b80d433eafd0fc89e1772343eb2805c2ad
Bkliger <bkliger@comcast.net>
2017-03-29 09:49:30 -0700

    set up rspec

--- Gemfile
+++ Gemfile
@@ -38,6 +38,13 @@ gem 'devise'
 gem 'friendly_id', '~> 5.1.0'
 
 gem 'will_paginate', '~> 3.1.0'
+
+gem 'rspec-rails'
+
+gem 'capybara'
+
+gem 'selenium-webdriver'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ spec/models/target_spec.rb
@@ -0,0 +1,8 @@
+# require 'rails_helper'
+
+RSpec.describe target, :type => :model do
+  it "is valid with valid attributes"
+  it "is not valid without a first_name"
+  it "is not valid without a last_name"
+  it "is not valid without a state"
+end
