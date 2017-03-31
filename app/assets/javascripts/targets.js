1f919852f2ecf8e5bfd9930b5388a9d1b23b2781
Bkliger <bkliger@comcast.net>
2017-03-31 12:57:27 -0700

    stuff

--- app/assets/javascripts/targets.js
+++ app/assets/javascripts/targets.js
@@ -1,19 +1,17 @@
 var ready = function() {
-  console.log("ready")
+//   var popup = new Foundation.Reveal($('#myModal'));
+// popup.open();
+// $(document).foundation();
+
+  // $('#exampleModal1').foundation('open');
   $("#mail").prop(':checked', true);
     $("#mail").attr(':checked', true);
-  console.log($("#mail").prop(':checked'))
-    console.log($("#mail").attr(':checked'))
-  console.log("checked")
   $("input[name='target[contact_method]']").click(function() {
-    console.log("clicked")
       if($('#email').is(':checked')) {
-        console.log("email is checked")
         $( "#email_input" ).removeClass( "hidden" );
         $( "#text_input" ).addClass( "hidden" );
       }
       else {
-        console.log("else")
         $( "#text_input" ).removeClass( "hidden" );
         $( "#email_input" ).addClass( "hidden" );
         $("#mail").prop('checked', false);
