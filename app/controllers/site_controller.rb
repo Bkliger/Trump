ef6c66e268e0c1502b1d4081835a738bfaaf5f05
Bkliger <bkliger@comcast.net>
2017-04-17 11:11:48 -0700

    spash page hot message

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -3,6 +3,8 @@ class SiteController < ApplicationController
     if current_user
         redirect_to targets_path
     else
+        main_org = Org.find_by org_name: 'General'
+        @hot_message = main_org.hot_message
         render :splash
     end
   end
